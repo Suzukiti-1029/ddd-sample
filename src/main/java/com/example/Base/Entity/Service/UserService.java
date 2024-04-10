f7e1f305e0d74f747b9fea3e26ad6e8fdfe6a008
Suzukiti-1029 <h40396suzujun@gmail.com>
2024-04-11 08:48:15 +0900

    メイン処理とUserドメインサービスのたたき台を作成

---
+++ src/main/java/com/example/Base/Entity/Service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.Base.Entity.Service;
+
+import com.example.Base.Entity.User;
+
+public class UserService {
+	public boolean Exists(User user) {
+		// return /* ユーザ重複チェック結果 */;
+		// ダミー
+		return false;
+	}
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import com.example.Base.Entity.User;
+import com.example.Base.Entity.Service.UserService;
+import com.example.Base.ValueObject.UserName;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) throws Exception {
+		Main main = new Main();
+		main.createUser(args[0]);
+	}
+
+	private void createUser(String userName) throws Exception {
+		User user = new User(new UserName(userName));
+		UserService userService = new UserService();
+		if (userService.Exists(user)) {
+			throw new Exception(userName + "はすでに存在しています。");
+		}
+		// ユーザ登録処理
+	}
 }
