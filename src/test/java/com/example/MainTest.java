39524a84837c2386f09b83a2287efb7b37e243a5
Suzukiti-1029 <h40396suzujun@gmail.com>
2024-04-12 09:25:10 +0900

    MainTestのメソッド名及びインメモリデータを確認するよう変更

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -1,19 +1,21 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import com.example.Base.Entity.Service.UserService;
 import com.example.Base.Repository.UserRepository;
 import com.example.Base.Repository.Impl.InMemoryUserRepository;
 
 public class MainTest {
 	@Test
-	public void createUserSuccessTest() throws Exception {
-		UserRepository uRepository = new InMemoryUserRepository();
-		UserService uService = new UserService(uRepository);
-		Main main = new Main(uRepository, uService);
-		assertDoesNotThrow(() -> main.createUser("suzuki"));
+	public void successCreateUserTest() throws Exception {
+		UserRepository userRepository = new InMemoryUserRepository();
+		Main main = new Main(userRepository);
+		main.createUser("suzuki");
+
+		String head = InMemoryUserRepository.store.values().stream()
+				.findFirst().map(user -> user.getName().getValue()).orElse("");
+		assertEquals("suzuki", head);
 	}
 }
