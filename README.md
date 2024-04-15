215cb4f063ede21de04386be05e3bc5f15cec5d6
Suzukiti-1029 <h40396suzujun@gmail.com>
2024-04-16 08:23:18 +0900

    テストおよびそのDB・設定を作成

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## データベースの設定
+### 開発用
 1. `mysql -u root -p`
 	- パスワード
 2. `create database ddd_sample;`
@@ -6,3 +7,11 @@
 4. `grant all privileges on ddd_sample.* to 'ddd_user'@'localhost';`
 5. `flush privileges;`
 6. `alter database ddd_sample character set utf8mb4 collate utf8mb4_unicode_ci;`
+
+### テスト用
+1. `mysql -u root -p`
+	- パスワード
+2. `create database ddd_sample_test;`
+3. `grant all privileges on ddd_sample_test.* to 'ddd_user'@'localhost';`
+5. `flush privileges;`
+6. `alter database ddd_sample_test character set utf8mb4 collate utf8mb4_unicode_ci;`
--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.Base.Domain.Entity.User;
 import com.example.Base.Domain.Repository.UserRepository;
+import com.example.Base.Domain.ValueObject.UserName;
+import com.example.Base.Infrastructure.HibernateUserRepository;
 import com.example.Base.Infrastructure.InMemoryUserRepository;
+import com.example.Base.Infrastructure.PersistenceFactoryManager;
 
 public class MainTest {
 	@Test
-	public void successCreateUserTest() throws Exception {
+	public void successCreateUserLogicTest() throws Exception {
 		UserRepository userRepository = new InMemoryUserRepository();
 		Main main = new Main(userRepository);
 		main.createUser("suzuki");
@@ -17,5 +21,25 @@ public class MainTest {
 		String head = InMemoryUserRepository.store.values().stream()
 				.findFirst().map(user -> user.getName().getValue()).orElse("");
 		assertEquals("suzuki", head);
+	}
+
+	@Test
+	public void successCreateUserDBTest() throws Exception {
+		PersistenceFactoryManager pfm = PersistenceFactoryManager.getInstance("myPUtest");
+		try {
+			UserRepository userRepository = new HibernateUserRepository(pfm);
+			Main main = new Main(userRepository);
+			main.createUser("suzuki");
+
+			User headUser = userRepository.find(new UserName("suzuki"));
+			String head = null;
+			if (headUser != null)
+				head = headUser.getName().getValue();
+			assertEquals("suzuki", head);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			pfm.close();
+		}
 	}
 }
